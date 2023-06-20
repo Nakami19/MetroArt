c3efd46b2df90b4138cd0062a858de9e29d7704d
Nakami <natalia.rivas@correo.unimet.edu.ve>
2023-06-20 12:27:28 -0400

    constantes agregadas

--- src/main.jsx
+++ src/main.jsx
@@ -4,21 +4,18 @@ import { HomePage } from './Pages/HomePage/HomePage'
 import './index.css'
 import {Routes, Route, BrowserRouter} from "react-router-dom";
 import Layout from './Components/Layout/Layout';
-import { HOME_URL } from './constants/url';
+import { ARTDETAIL_URL, HOME_URL, TOURS_URL } from './constants/url';
 import { ToursPage } from './Pages/ToursPage/ToursPage';
 import { ArtDetailsPage } from './Pages/ArtDetailsPage/ArtDetailsPage';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-    <BrowserRouter> {/*es como una pila de url*/}
-    <Routes>{/*desde aqui se empiezan a declarar las diferentes rutas de la pagina*/}
-      <Route element={<Layout/>}>
-        <Route path={HOME_URL} element={<HomePage/>}/>  {/*cada route es una ruta de la pagina*/}
-      </Route>
+    <BrowserRouter> 
+    <Routes>
     <Route element={<Layout/>}>
-        <Route path="/" element={<HomePage/>}/>  {/*cada route es una ruta de la pagina*/}
-        <Route path='/tours' element={<ToursPage/>}/>
-        <Route path='/artdetails' element={<ArtDetailsPage/>}/>
+        <Route path={HOME_URL} element={<HomePage/>}/> 
+        <Route path={TOURS_URL} element={<ToursPage/>}/>
+        <Route path={ARTDETAIL_URL} element={<ArtDetailsPage/>}/>
     </Route>
     </Routes>
     </BrowserRouter>
