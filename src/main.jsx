2d9c3e7dd0c55a484b83be5b7d84a5631d69f5bf
mginez <mginez@correo.unimet.edu.ve>
2023-06-20 19:37:15 -0400

    signup y login (por favor funciona)

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { HomePage } from './Pages/HomePage/HomePage'
+import { SignupPage } from './Pages/SignupPage/SignupPage'
+import { LoginPage } from './Pages/LoginPage/LoginPage'
+import { ProfilePage } from './Pages/ProfilePage/ProfilePage'
+import './index.css'
+import {Routes, Route, BrowserRouter} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <h1>Todo suyo mi ladies Paula y Marielena</h1>
+    <React.StrictMode>
+    <BrowserRouter> {/*es como una pila de url*/}
+    <Routes>{/*desde aqui se empiezan a declarar las diferentes rutas de la pagina*/}
+      <Route path="/" element={<HomePage/>}  />  {/*cada route es una ruta de la pagina*/}
+      <Route path="/signup" element={<SignupPage/>}  /> 
+      <Route path="/login" element={<LoginPage/>}  /> 
+      <Route path="/profile" element={<ProfilePage/>}  /> 
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
+
 )
