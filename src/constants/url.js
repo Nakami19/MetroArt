dffefa9d21fa94a3a52d0e9b33d4281462129b77
Paula Alonso <paula.alonso@correo.unimet.edu.ve>
2023-06-20 17:56:51 -0400

    Nueva Url

--- src/constants/url.js
+++ src/constants/url.js
@@ -4,3 +4,4 @@ export const REGISTER_URL = "/register";
 export const PROFILE_URL = "/profile";
 export const TOURS_URL = '/tours'
 export const ARTDETAIL_URL='/artdetails'
+export const TOURDETAILS_URL='/tourdetails'
