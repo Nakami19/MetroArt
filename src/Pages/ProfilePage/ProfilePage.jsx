80726001f557187e73405320496414fe0a21c445
mginez <mginez@correo.unimet.edu.ve>
2023-06-21 12:39:27 -0400

    merge

--- src/Pages/ProfilePage/ProfilePage.jsx
+++ src/Pages/ProfilePage/ProfilePage.jsx
@@ -17,47 +17,31 @@ export function ProfilePage() {
                 <div class="bg-white overflow-hidden shadow rounded-lg border">
                     <div class="px-4 py-5 sm:px-6">
                         <h3 class="text-lg leading-6 font-medium text-gray-900">
-                            User Profile
+                            Perfil de usuario
                         </h3>
                         <p class="mt-1 max-w-2xl text-sm text-gray-500">
-                            This is some information about the user.
+                            Visitante
                         </p>
                     </div>
                     <div class="border-t border-gray-200 px-4 py-5 sm:p-0">
                         <dl class="sm:divide-y sm:divide-gray-200">
                             <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                 <dt class="text-sm font-medium text-gray-500">
-                                    Full name
+                                    Nombre
                                 </dt>
                                 <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                    John Doe
+                                    Tu nombre
                                 </dd>
                             </div>
                             <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                 <dt class="text-sm font-medium text-gray-500">
-                                    Email address
+                                    Correo electrónico
                                 </dt>
                                 <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                    johndoe@example.com
+                                    tucorreo@example.com
                                 </dd>
                             </div>
-                            <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                <dt class="text-sm font-medium text-gray-500">
-                                    Phone number
-                                </dt>
-                                <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                    (123) 456-7890
-                                </dd>
-                            </div>
-                            <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                <dt class="text-sm font-medium text-gray-500">
-                                    Address
-                                </dt>
-                                <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                    123 Main St
-                                    Anytown, USA 12345
-                                </dd>
-                            </div>
+                            
                         </dl>
                     </div>
             </div>
