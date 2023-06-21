710bc4148783695f0c0b6a2d3c8167ac84634416
mginez <mginez@correo.unimet.edu.ve>
2023-06-21 16:44:20 -0400

    merge

--- src/constants/url.js
+++ src/constants/url.js
@@ -4,8 +4,5 @@ export const REGISTER_URL = "/register";
 export const PROFILE_URL = "/profile";
 export const TOURS_URL = '/tours'
 export const ARTDETAIL_URL='/artdetails'
-<<<<<<< HEAD
-export const TOURDETAILS_URL='/tourdetails'
-=======
 export const TOURDETAILS_URL='/tourdetails'
 export const RESERVATION_URL  = '/reserve'
