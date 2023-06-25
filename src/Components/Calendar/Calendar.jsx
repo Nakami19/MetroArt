e5d76d3424e74f1008a9c2dc3e4f7d020bc9fa52
BomeLoL <kli@correo.unimet.edu.ve>
2023-06-25 09:59:33 -0400

    validacion de calendario

--- src/Components/Calendar/Calendar.jsx
+++ src/Components/Calendar/Calendar.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react';
+import dayjs from 'dayjs';
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
+export function Calendar({ onDateSelect }) {
+  const [selectedDate, setSelectedDate] = useState(null);
+  const today = dayjs().startOf('day');
+  const maxDate = today.add(1, 'month'); 
+  const minDate = today; 
 
-export function Calendar() {
+  const handleDateChange = (date) => {
+    if (dayjs(date).isBetween(minDate, maxDate, null, '[]')) { 
+      setSelectedDate(date);
+      onDateSelect(date);
+    }
+  };
 
   return (
     <div>
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DemoContainer components={['DatePicker']}>
-        <DatePicker label="Día de la reserva"/>
-      </DemoContainer>
-    </LocalizationProvider>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DemoContainer components={['DatePicker']}>
+          <DatePicker label="Día de la reserva" value={selectedDate} onChange={handleDateChange} minDate={minDate} maxDate={maxDate} />
+        </DemoContainer>
+      </LocalizationProvider>
     </div>
-  )
+  );
 }
