d5215ba2ec1454fe67ade89aa3f4dd1daf382972
Haidar Bazzi <bazzi.haidar@correo.unimet.edu.ve>
2023-07-02 14:25:05 -0400

    probando

--- src/Components/PaypalWrapper/PaypalWrapper.jsx
+++ src/Components/PaypalWrapper/PaypalWrapper.jsx
@@ -1,11 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import {
     PayPalButtons,
     usePayPalScriptReducer
 } from "@paypal/react-paypal-js";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 export function PaypalWrapper ({ currency, pay}) {
     const [{ options, isPending }, dispatch] = usePayPalScriptReducer();
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     useEffect(() => {
         dispatch({
@@ -23,6 +29,26 @@ export function PaypalWrapper ({ currency, pay}) {
         style={{"layout":"vertical","label":"donate", 'color':'silver'}}
         disabled={false}
         forceReRender={[pay]}
+        onClick={ function(){
+            setShow(true)
+            return(
+                <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                  <Button variant="secondary" onClick={handleClose}>
+                    Close
+                  </Button>
+                  <Button variant="primary" onClick={handleClose}>
+                    Save Changes
+                  </Button>
+                </Modal.Footer>
+              </Modal>
+            )
+        }
+        }
         createOrder={(data, actions) => {
             return actions.order
                 .create({
@@ -52,10 +78,10 @@ export function PaypalWrapper ({ currency, pay}) {
                     ],
                 })
                 .then((orderId) => {
-                    alert("PAGADO POR PAYPAL")
                     return orderId;
-                });
+                })
         }}
+        
     />
      );
 }
